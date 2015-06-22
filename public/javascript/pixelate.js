9dbc4f12450d2924bd41e426f63576f9150dd047
Paul Alvarez <pclalv@gmail.com>
2015-06-22 16:53:33 -0400

    change pixelate to pixelate in 16 colors

--- public/javascript/pixelate.js
+++ public/javascript/pixelate.js
@@ -1,43 +1,97 @@
-$(function () {
-  if (typeof Pixelate === "undefined") {
+(function () {
+  if (typeof Pixelate === 'undefined') {
     window.Pixelate = {};
   }
 
-  $.Pixelate = function () {
+  $.Pixelate = function (options) {
     var my = this;
 
-    this.scale = 1/6,
-    this.img = new Image(),
-    this.canvas = document.getElementById('canvas'),
-    this.scaledWidth = canvas.width*this.scale,
-    this.scaledHeight = canvas.height*this.scale,
-    this.ctx = canvas.getContext('2d');
+    this.stepSize = 16;
+    this.transitionStep = [];
 
-    this.img.src = 'public/images/me.jpg';
+    this.scale = 1/6;
+    this.img = new Image();
+    this.canvas = options.el;
+    this.scaledWidth = this.canvas.width*this.scale;
+    this.scaledHeight = this.canvas.height*this.scale;
+    this.ctx = this.canvas.getContext('2d');
+
+    this.img.src = this.canvas.getAttribute('pixelate-src');
 
     this.img.onload = function () {
       my.ctx.drawImage(my.img, 0, 0, my.scaledWidth, my.scaledHeight);
       my.ctx.drawImage(my.canvas, 0, 0, my.scaledWidth, my.scaledHeight, 0, 0, my.canvas.width, my.canvas.height);
 
-      var imgPixels = my.ctx.getImageData(0, 0, my.canvas.width, my.canvas.height);
-      for(var y = 0; y < imgPixels.height; y++){
-          for(var x = 0; x < imgPixels.width; x++){
-              var i = (y * 4) * imgPixels.width + x * 4; //Why is this multiplied by 4?
-              var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3; //Is this getting the average of the values of each channel R G and B, and converting them to BW(?)
-              imgPixels.data[i] = avg;
-              imgPixels.data[i + 1] = avg;
-              imgPixels.data[i + 2] = avg;
-          }
-      }
-      my.ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-    }
+      my.averagePixels();
+    };
 
     this.ctx.mozImageSmoothingEnabled = false;
-    this.ctx.webkitImageSmoothingEnabled = false;
     this.ctx.imageSmoothingEnabled = false;
+
+    this.bindEvents();
   };
-});
+
+  $.Pixelate.prototype.averagePixels = function () {
+    var x, y, i, j, avg, roundedAvg;
+
+    var imgPixels = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    for (y = 0; y < imgPixels.height; y++) {
+      for (x = 0; x < imgPixels.width; x++) {
+        i = (y * 4) * imgPixels.width + x * 4;
+        avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+        roundedAvg = Math.floor(avg/16) * 16;
+
+        for (j = 0; j <= 2; j++) {
+          // to transition we need to store an amount to increment by evenly at each step
+          this.transitionStep[i + j] = (imgPixels.data[i + j] - roundedAvg)/this.stepSize;
+
+          // setting the image to sixteen grayscale
+          imgPixels.data[i + j] = roundedAvg;
+        }
+      }
+    }
+
+    this.ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+  };
+
+  $.Pixelate.prototype.bindEvents = function () {
+    var x, y, i, j, avg, roundedAvg,
+        my = this;
+
+    this.canvas.addEventListener("mouseenter", function (event) {
+      my.ctx.drawImage(my.img, 0, 0, my.scaledWidth, my.scaledHeight);
+      my.ctx.drawImage(my.canvas, 0, 0, my.scaledWidth, my.scaledHeight, 0, 0, my.canvas.width, my.canvas.height);
+
+      var imgPixels = my.ctx.getImageData(0, 0, my.canvas.width, my.canvas.height);
+      for (y = 0; y < imgPixels.height; y++) {
+        for (x = 0; x < imgPixels.width; x++) {
+          i = (y * 4) * imgPixels.width + x * 4;
+          avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+          roundedAvg = Math.floor(avg/16) * 16;
+
+          for (j = 0; j <= 2; j++) {
+            // to transition we need to store an amount to increment by evenly at each step
+            my.transitionStep[i + j] = (imgPixels.data[i + j] - roundedAvg)/my.stepSize;
+
+            // setting the image to sixteen grayscale
+            imgPixels.data[i + j] = roundedAvg;
+          }
+        }
+      }
+
+      my.ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+    });
+  };
+
+  $.fn.pixelate = function () {
+    return this.each(function () {
+      new $.Pixelate({
+        el: this
+      });
+    });
+  };
+})();
 
 $(function () {
-  $.Pixelate();
-})
+  $('.pixelate').pixelate();
+});
